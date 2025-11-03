6bc004d42c61a6cd86554e31d6cf27baf704ffd0
Hayley McVicar <hrmcvicar@gmail.com>
2025-11-03 13:07:49 -0800

    player add and delete functions added

--- index1.js
+++ index1.js
@@ -10,6 +10,8 @@ let singlePlayer = null;
 
 const allPlayers = document.querySelector("#allPlayers");
 const singlePlayerDiv = document.querySelector("#singlePlayer");
+const addNewPlayerForm = document.querySelector("#newPlayerForm");
+console.log("players in render:", players);
 
 const render = () => {
   const html = players.map((player) => {
@@ -26,6 +28,7 @@ const render = () => {
   <div>
     <h2>${singlePlayer.name}</h2>
     <h2>${singlePlayer.breed}</h2>
+    <button class="playerDelete" data-id="${singlePlayer.id}">Delete</button>
   </div>
     `
     : "Click a player to learn more!";
@@ -68,5 +71,51 @@ allPlayers.addEventListener("click", async (event) => {
       return player.id === id;
     });
     render();
+  }
+});
+
+addNewPlayerForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(addNewPlayerForm);
+  const newPlayer = {
+    name: formData.get("name"),
+    breed: formData.get("breed"),
+  };
+  try {
+    const response = await fetch(
+      "https://fsa-puppy-bowl.herokuapp.com/api/2510-HAYLEY/players",
+      {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(newPlayer),
+      }
+    );
+    const data = await response.json();
+    players.push(data.data.players);
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+singlePlayerDiv.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("playerDelete")) {
+    const id = event.target.getAttribute("data-id") * 1;
+    console.log(id);
+    try {
+      await fetch(
+        `https://fsa-puppy-bowl.herokuapp.com/api/2510-HAYLEY/players/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      players = players.filter((player) => {
+        return player.id !== id;
+      });
+      singlePlayer = null;
+      render();
+    } catch (error) {
+      console.error(error);
+    }
   }
 });
