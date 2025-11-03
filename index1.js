6f01d86d2866f583fb6246b23d23001709a4f53a
Hayley McVicar <hrmcvicar@gmail.com>
2025-11-03 12:26:28 -0800

    added select player event listener correctly

--- index1.js
+++ index1.js
@@ -6,7 +6,7 @@ const API = BASE + COHORT + RESOURCE;*/
 
 //state
 let players = [];
-let singlePlayer;
+let singlePlayer = null;
 
 const allPlayers = document.querySelector("#allPlayers");
 const singlePlayerDiv = document.querySelector("#singlePlayer");
@@ -20,6 +20,7 @@ const render = () => {
             `;
   });
   allPlayers.innerHTML = html.join("");
+
   singlePlayerDiv.innerHTML = singlePlayer
     ? `
   <div>
@@ -34,11 +35,12 @@ const render = () => {
 const fetchAllPlayers = async () => {
   try {
     const response = await fetch(
-      "https://fsa-puppy-bowl.herokuapp.com/api/2510-hayley/players"
+      "https://fsa-puppy-bowl.herokuapp.com/api/2510-HAYLEY/players"
     );
-    const data = await response.json();
-    console.log(data.data);
-    players = data.data;
+    const json = await response.json();
+    console.log(json.data.players); //neded json!
+    players = json.data.players;
+    render();
   } catch (error) {
     console.error(error);
   }
@@ -50,16 +52,21 @@ allPlayers.addEventListener("click", async (event) => {
   if (event.target.classList.contains("pName")) {
     const id = event.target.getAttribute("data-id") * 1;
     console.log(id);
-  }
-  try {
+
+    /*  try {
     const response = await fetch(
-      "https://fsa-puppy-bowl.herokuapp.com/api/2510-hayley/players/{id}"
+      "https://fsa-puppy-bowl.herokuapp.com/api/2510-HAYLEY/players/{id}"
     );
     const data = await response.json();
     console.log(data.data);
     singlePlayer = data.data;
     render();
   } catch (error) {
-    console.error(error);
+    console.error(error);*/
+    //I couldnt get try to work so going w find
+    singlePlayer = players.find((player) => {
+      return player.id === id;
+    });
+    render();
   }
 });
